f6102b0d4d7822f8522cbeaf3058ca9cda2564c5
Sun Kai <moguizhiz@126.com>
2021-11-12 20:14:18 +0800

    代码可读性修改

--- src/rl/graph_search/pn.py
+++ src/rl/graph_search/pn.py
@@ -97,11 +97,11 @@ class GraphSearchPolicy(nn.Module):
         state_embeddings = self.W2(state_embeddings)
         state_embeddings = self.W2Dropout(state_embeddings)
 
-        def policy_nn_fun(X2, action_space):
+        def policy_nn_fun(state_embeddings, action_space):
             (r_space, e_space), action_mask = action_space
             A = self.get_action_embedding((r_space, e_space), kg)
             action_dist = F.softmax(
-                torch.squeeze(A @ torch.unsqueeze(X2, 2), 2) - (1 - action_mask) * ops.HUGE_INT, dim=-1)
+                torch.squeeze(A @ torch.unsqueeze(state_embeddings, 2), 2) - (1 - action_mask) * ops.HUGE_INT, dim=-1)
             # action_dist = ops.weighted_softmax(torch.squeeze(A @ torch.unsqueeze(X2, 2), 2), action_mask)
             return action_dist, ops.entropy(action_dist)
 
