0a3c620d177f0a5254dbdcbffcbb3c8eeeb714a7
Nurassyl-lab <nurassyl.askar@gmail.com>
2024-10-21 17:22:58 +0800

    WID: Added important function to pg.py and pn.py
    Functions are: sample_action, sample, similarity_check, get_action_space, policy_nn_fun

--- multihopkg/rl/graph_search/pg.py
+++ multihopkg/rl/graph_search/pg.py
@@ -252,7 +252,8 @@ class PolicyGradient(LFramework):
         :return next_action (next_r, next_e): Sampled next action.
         :return action_prob: Probability of the sampled action.
         """
-
+        
+        #! This function can be removed
         def apply_action_dropout_mask(action_dist, action_mask):
             if self.action_dropout_rate > 0:
                 rand = torch.rand(action_dist.size())
@@ -266,17 +267,50 @@ class PolicyGradient(LFramework):
             else:
                 return action_dist
 
+        #* Added
+        def similarity_check(x_space, continuous_action):
+            """
+            Find what x_space elements is similar to continuous_action.
+
+            Input shapes:
+                x_space:            torch.Size([batch_size*num_rollouts, num_actions+1])
+                continuous_action:  torch.Size([batch_size*num_rollouts, num_actions])
+
+            Return:
+                torch.Size([batch_size*num_rollouts])
+            """
+            # Check what element in the x_space is similar to the continuous_action
+            # To do so, use the cosine similarity
+
+            # padd continuous_action with zeros, on device
+            device = 'cuda'
+            continuous_action = torch.cat((continuous_action, torch.zeros(continuous_action.shape[0], 1).to(device)), dim=1)
+
+            x_space = x_space.view(-1, 1)
+            differences = x_space - continuous_action
+            similarity_score = torch.sum(differences, dim=1)
+            most_similar = torch.argmax(similarity_score)
+
+            x_space_element = x_space[most_similar]
+            return x_space_element
+
+        #* Updated
         def sample(action_space, action_dist):
-            sample_outcome = {}
             ((r_space, e_space), action_mask) = action_space
-            sample_action_dist = apply_action_dropout_mask(action_dist, action_mask)
-            idx = torch.multinomial(sample_action_dist, 1, replacement=True)
-            next_r = ops.batch_lookup(r_space, idx)
-            next_e = ops.batch_lookup(e_space, idx)
-            action_prob = ops.batch_lookup(action_dist, idx)
+
+            actions = action_dist.rsample()
+            next_r = similarity_check(r_space, actions)
+            next_e = similarity_check(e_space, actions)
+
+            log_prob = action_dist.log_prob(actions).sum(dim=-1)
+
+            sample_outcome = {}
             sample_outcome['action_sample'] = (next_r, next_e)
-            sample_outcome['action_prob'] = action_prob
-            return sample_outcome
+            sample_outcome['action_prob'] = log_prob
+            log_prob = action_dist.log_prob(actions).sum(dim=-1)
+            entropy = action_dist.entropy().sum(dim=-1)
+
+            return sample_outcome, entropy
 
         if inv_offset is not None:
             next_r_list = []
