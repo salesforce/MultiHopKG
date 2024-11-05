718e2c833d155b2fd03889e48b4708bc0c62659c
ottersome <luisgarcia26284@gmail.com>
2024-11-05 17:26:25 +0800

    Removing duplicate graph

--- multihopkg/environments.py
+++ multihopkg/environments.py
@@ -12,19 +12,3 @@ class Environment(ABC):
         pass
 
 
-class QnAGraphEnvironment(Environment):
-     def __init__(self):
-
-        # NOTE: :We dont really need embeddings here since we are just sampling at random. 
-        # TODO: But herpahs we want to get a ANN search here. Since once the action is taken we want to get the closes neighbor. 
-        pass
-
-    def reset(self):
-        pass
-
-    def step(self):
-        return
-
-    def _find_closest_neighbor(self, position: torch.Tensor):
-        raise NotImplementedError
-    
